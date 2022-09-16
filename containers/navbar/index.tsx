1b28e49cbf413e382ff3eb91cc96e06fa87cb5db
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-09-16 15:08:09 -0700

    Navbar designed

--- containers/navbar/index.tsx
+++ containers/navbar/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="w-full sticky top-0 bg-stone-800 text-slate-100">
+      <div className="container mx-auto py-5 flex items-center justify-between">
+          <span className="text-2xl font-semibold uppercase">Big Oink Studios</span>
+          <span className="text-xl text-white uppercase">Portfolio</span>
+      </div>
+    </nav> 
+  )
 };
 
 export default Navbar;

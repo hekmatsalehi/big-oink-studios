6b14b1bf3f82924f3ee9cc5188ea621ad94ce333
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-09-16 16:55:31 -0700

    Hero added

--- containers/navbar/index.tsx
+++ containers/navbar/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="w-full sticky top-0 bg-stone-800 text-slate-100">
+    <nav className="w-full sticky top-0 z-10 bg-stone-800 text-slate-100">
       <div className="container mx-auto py-5 flex items-center justify-between">
           <span className="text-2xl font-semibold uppercase">Big Oink Studios</span>
           <span className="text-xl text-white uppercase">Portfolio</span>

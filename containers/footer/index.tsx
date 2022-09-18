576490bef8bb2aad808ab00942dd746b493a9283
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-09-17 21:46:58 -0700

    Footer link added

--- containers/footer/index.tsx
+++ containers/footer/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = ({ type }) => {
@@ -5,9 +6,17 @@ const Footer = ({ type }) => {
     <footer className="w-full bg-[#111111] text-slate-100">
       <div className="container mx-auto pt-11 pb-12 flex items-end  justify-between">
         <div className="flex flex-col gap-2">
-          <span className="text-2xl font-semibold uppercase">
-            {type === "home" ? "Noble Ocean Media" : "Big Oink Studios"}
-          </span>
+          <Link href="/">
+            <span
+              className={
+                type === "home"
+                  ? "text-2xl font-semibold uppercase cursor-pointer"
+                  : "text-2xl font-normal uppercase cursor-pointer"
+              }
+            >
+              {type === "home" ? "Noble Ocean Media" : "Big Oink Studios"}
+            </span>
+          </Link>
           <span className="text-xs font-semibold">
             {type === "home"
               ? "WORK HARD, PLAY HARD, REST HARD"

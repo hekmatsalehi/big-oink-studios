ffef306eef32b9ebf2e50de8663cc873217517cd
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-09-17 22:23:02 -0700

    Contact Us button hover effect

--- components/contactButton.tsx
+++ components/contactButton.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const ContactButton = () => {
   return (
     <Link href="/#contact">
-      <button className="w-fit bg-[#74BFC4] text-[#FFFFFF] font-medium px-5 py-2 cursor-pointer">
+      <button className="w-fit bg-[#74BFC4] text-[#FFFFFF] font-medium px-5 py-2 cursor-pointer hover:bg-[#2fb4bd] ease-out duration-200">
         CONTACT US
       </button>
     </Link>
